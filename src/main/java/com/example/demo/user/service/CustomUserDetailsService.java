5d931822c5baa618d60edb53a7c408ffbb7a3d77
rlarbtns5898-design <rlarbtns5898@gmail.com>
2026-02-12 18:51:13 +0900

    first commit

--- src/main/java/com/example/demo/user/service/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/user/service/CustomUserDetailsService.java
@@ -1,4 +1,30 @@
 package com.example.demo.user.service;
 
-public class CustomUserDetailsService {
+import com.example.demo.user.entity.User;
+import com.example.demo.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("없는 아이디입니다."));
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
 }
