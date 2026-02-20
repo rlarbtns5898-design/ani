44ed584dde57d23b28a7e5de37f6b38923dae513
rlarbtns5898-design <rlarbtns5898@gmail.com>
2026-02-20 21:18:55 +0900

    animecontroller 오류 수정

--- src/main/java/com/example/demo/user/controller/AnimeController.java
+++ src/main/java/com/example/demo/user/controller/AnimeController.java
@@ -1,11 +1,13 @@
 package com.example.demo.user.controller;
 
-import org.hibernate.mapping.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 
 @Controller
