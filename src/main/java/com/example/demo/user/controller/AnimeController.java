d4e3dfc9e29cf41af23993f7047d036a8c47caaa
rlarbtns5898-design <rlarbtns5898@gmail.com>
2026-02-20 21:00:46 +0900

    AnimeController 수정

--- src/main/java/com/example/demo/user/controller/AnimeController.java
+++ src/main/java/com/example/demo/user/controller/AnimeController.java
@@ -1,11 +1,14 @@
 package com.example.demo.user.controller;
 
 import org.hibernate.mapping.Map;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
+
+@Controller
 public class AnimeController {
     @GetMapping("/anime")
     public String animeSearchPage() {
