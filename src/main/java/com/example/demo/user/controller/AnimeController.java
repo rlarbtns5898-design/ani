0cbdfad45f496d7f727f8d280ae448d292004058
rlarbtns5898-design <rlarbtns5898@gmail.com>
2026-02-20 20:33:44 +0900

    anime 검색 추가

---
+++ src/main/java/com/example/demo/user/controller/AnimeController.java
@@ -0,0 +1,26 @@
+package com.example.demo.user.controller;
+
+import org.hibernate.mapping.Map;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+public class AnimeController {
+    @GetMapping("/anime")
+    public String animeSearchPage() {
+        return "anime_search";
+    }
+    @GetMapping("/anime/search")
+    public String searchAnime(@RequestParam String keyword, Model model) {
+
+        String url = "https://api.jikan.moe/v4/anime?q=" + keyword;
+
+        RestTemplate restTemplate = new RestTemplate();
+        Map response = restTemplate.getForObject(url, Map.class);
+
+        model.addAttribute("result", response);
+
+        return "anime_result";
+    }
+}
