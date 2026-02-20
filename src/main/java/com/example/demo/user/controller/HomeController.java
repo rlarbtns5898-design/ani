bf13d326b5353a2a88249c10053d248d4577e487
rlarbtns5898-design <rlarbtns5898@gmail.com>
2026-02-20 20:00:42 +0900

    home 오류mapping 수정

--- src/main/java/com/example/demo/user/controller/HomeController.java
+++ src/main/java/com/example/demo/user/controller/HomeController.java
@@ -6,8 +6,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
-    public String home() {
-        return "home";
-    }
+
 }
